0a18641fb690abc8eea169161af865fe5ee55c03
hpca01 <hirenpatelca@gmail.com>
2020-11-01 18:42:08 -0800

    removed whitenoise

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 CLOUDINARY_STORAGE = {
     "CLOUD_NAME": "hzjlg6kbk",
